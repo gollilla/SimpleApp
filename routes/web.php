7ee9b8813dedf4546d15caccd65f68647501e11d
gollilla <soradore2@gmail.com>
2025-05-31 23:10:34 +0900

    update: CLAUDE調整

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/components', function () {
+    return Inertia::render('Components');
+});
+
 require __DIR__.'/auth.php';
